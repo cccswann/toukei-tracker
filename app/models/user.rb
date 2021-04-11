e8c120549d6c4f91140a8a07573f9651681370c4
cccswann <cccswann@gmail.com>
2021-04-11 15:56:26 -0700

    Add User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :time_entries, dependent: :destroy
+    has_many :tags, dependent: :destroy
+    has_many :tasks, dependent: :destroy
+    has_many :goals, dependent: :destroy
 end
