98bd2f47776cc0c49cd11b0ff49dda0ff1949c91
cccswann <cccswann@gmail.com>
2021-04-11 15:56:50 -0700

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+cici = User.create!(name: "cccswann", email: "cccswann@gmail.com", password: "herpaDerpa")
+
+cici_tags = cici.tags.create!(
+    [
+        {name: "Listening"},
+        {name: "Reading"},
+        {name: "Shadowing"}
+    ]
+)
