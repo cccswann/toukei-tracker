7d639e0193d8ee32407a9fe671dcfe290d1177e1
cccswann <cccswann@gmail.com>
2021-04-11 16:40:06 -0700

    Update schema with foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_224416) do
+ActiveRecord::Schema.define(version: 2021_04_11_233749) do
 
   create_table "goals", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["task_id"], name: "index_taggings_on_task_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -30,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_04_11_224416) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "time_entries", force: :cascade do |t|
@@ -48,5 +61,9 @@ ActiveRecord::Schema.define(version: 2021_04_11_224416) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "goals", "users"
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "taggings", "tasks"
   add_foreign_key "tags", "users"
+  add_foreign_key "tasks", "users"
 end
