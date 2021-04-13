c92370d952f37e219d0afcbeea24d84d37988e92
cccswann <cccswann@gmail.com>
2021-04-13 16:37:44 -0700

    Add method to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :tags, dependent: :destroy
     has_many :tasks, dependent: :destroy
     has_many :goals, dependent: :destroy
+
+    has_secure_password
 end
